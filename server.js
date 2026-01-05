8fd2615c6223b970a5dfe27e7703275d567f25e4
mr-dark <motedee02@gmail.com>
2026-01-05 21:40:35 +0700

    up

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', (req,res)=>{
     res.end("Welcome to the Cambodia Postal Code Project!");
