05999de93bf79d9d7756bba1265ad5498cd28f7d
mr-dark <motedee02@gmail.com>
2026-01-05 21:20:44 +0700

    up

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req,res)=>{
 const fs = require('fs');
 const path = require('path');
 
-app.get('/api/cam-postal-code', async (req, res) => {
+app.get('/api/en/cam-postal-code', async (req, res) => {
     const name = req.query.name;
 
     if (!name) {
@@ -17,6 +17,37 @@ app.get('/api/cam-postal-code', async (req, res) => {
 
     try {
         const filePath = path.join(__dirname, 'data/cambodia-postal-code.json');
+
+        // Read file async
+        const rawData = fs.readFileSync(filePath, 'utf8');
+        const postalCode = JSON.parse(rawData);
+
+        const result = postalCode.filter(
+            code => code.name.toLowerCase() === name.toLowerCase()
+        );
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'No postal code found' });
+        }
+
+        // Send response ONCE
+        return res.json(result);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.get('/api/kh/cam-postal-code', async (req, res) => {
+    const name = req.query.name;
+
+    if (!name) {
+        return res.status(400).json({ message: 'name is required' });
+    }
+
+    try {
+        const filePath = path.join(__dirname, 'data/cambodia-postal-code-kh.json');
 
         // Read file async
         const rawData = fs.readFileSync(filePath, 'utf8');
